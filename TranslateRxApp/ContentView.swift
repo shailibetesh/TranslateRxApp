aa82779f8a518586dade3b64269b14972796ba2b
Shaili Betesh <shailibetesh@gmail.com>
2024-08-20 20:56:25 -0700

    Initial Commit

---
+++ TranslateRxApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TranslateRxApp
+//
+//  Created by Shaili Betesh on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TranslateRxApp/TranslateRxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateRxAppApp.swift
+//  TranslateRxApp
+//
+//  Created by Shaili Betesh on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslateRxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
